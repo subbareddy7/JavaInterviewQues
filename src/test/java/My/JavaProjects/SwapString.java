ca7448901cf10601b0e093a8ac2a45c360335bdf
AG79469 <AG79469@LD1GH533.us.ad.wellpoint.com>
2021-07-05 18:50:20 +0530

    Swap String update

--- src/test/java/My/JavaProjects/SwapString.java
+++ src/test/java/My/JavaProjects/SwapString.java
@@ -5,11 +5,16 @@ public class SwapString {
 	public static void main(String args[]) {
 		String a="Subba";
 		String b="ReddyA";
+		String c="";
 		System.out.println("Before Swap of A:"+a);
 		System.out.println("Before Swap of B:"+b);
-		a=a+b;
-		b=a.substring(0,a.length()-b.length());
-		a=a.substring(b.length());
+		  a=a+b;
+		  b=a.substring(0,a.length()-b.length());
+		  a=a.substring(b.length());
+		//Simplest Way
+		/*
+		 * c=a; a=b; b=c;
+		 */
 		System.out.println("*****After Swap*****");
 		System.out.println("Before Swap of A:"+a);
 		System.out.println("Before Swap of B:"+b);
